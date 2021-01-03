1095a6a64bbe794179502093a8e6f092cc3e0e32
Ali Dogan <32383093+doganali7@users.noreply.github.com>
2021-01-03 21:19:32 +0200

    Add files via upload

---
+++ cpu_scheduler_simulator.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <stdlib.h>
+
+using namespace std;
+
+int main()
+{
+    cout<<"CPU Scheduler Simulator \n";
+    cout<<"Scheduling Method \n";
+    cout<<"Preemtive Mode \n";
+    cout<<"Non-Preemptive Mode \n";
+    cout<<"Show Result \n";
+    cout<<"End Program \n";
+
+    return 0;
+}
